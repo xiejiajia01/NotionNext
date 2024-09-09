80d1b02fca7b7e49ffd9c2665900df8997c8e63f
xiejiajia01 <160386957+xiejiajia01@users.noreply.github.com>
2024-09-09 23:31:18 +0800

    Update Footer.js
    备案-3

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -11,21 +11,45 @@ export default function Footer (props) {
   const currentYear = d.getFullYear()
   const since = siteConfig('SINCE')
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
+//原配置
+  // return <footer className="relative w-full bg-black px-6 border-t">
+  //       <DarkModeButton className='text-center pt-4'/>
 
-  return <footer className="relative w-full bg-black px-6 border-t">
-        <DarkModeButton className='text-center pt-4'/>
+  //       <div className="text-yellow-300 container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
+  //           <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
+  //           <div className="md:p-0 text-center md:text-right text-xs">
+  //               {/* 右侧链接 */}
+  //               {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
+  //               {siteConfig('BEI_AN') && (<a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{siteConfig('BEI_AN')} </a>)}
+  //               <span className='no-underline ml-4'>
+  //                   Powered by
+  //                   <a href="https://github.com/tangly1024/NotionNext" className=' hover:underline'> NotionNext {siteConfig('VERSION')}  </a>
+  //               </span>
+  //           </div>
+  //       </div>
+  //   </footer>
+  return (
+  <footer className="relative w-full bg-black px-6 border-t">
+    <DarkModeButton className='text-center pt-4' />
 
-        <div className="text-yellow-300 container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
-            <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
-            <div className="md:p-0 text-center md:text-right text-xs">
-                {/* 右侧链接 */}
-                {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
-                {siteConfig('BEI_AN') && (<a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{siteConfig('BEI_AN')} </a>)}
-                <span className='no-underline ml-4'>
-                    Powered by
-                    <a href="https://github.com/tangly1024/NotionNext" className=' hover:underline'> NotionNext {siteConfig('VERSION')}  </a>
-                </span>
-            </div>
-        </div>
-    </footer>
+    <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
+      <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
+      <div className="md:p-0 text-center md:text-right text-xs">
+        {/* 右侧链接 */}
+        {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
+        <a href="https://beian.miit.gov.cn/#/Integrated/index" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">
+          黔ICP备2022008054号-1
+        </a>
+        <span className='no-underline ml-4'>
+          Powered by
+          <a href="https://github.com/tangly1024/NotionNext" className='hover:underline'>
+            NotionNext {siteConfig('VERSION')}
+          </a>
+        </span>
+      </div>
+    </div>
+  </footer>
+);                                                                                                                                                                    
+                                                                                                                                                                       
+                                                                                                                                                                       
 }
